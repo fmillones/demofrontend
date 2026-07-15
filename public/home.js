ec6696d8c158eba975eb633cab5acbfdd3d82abd
Fabian <fabian.millones@lyra.com>
2026-07-14 19:11:19 -0500

    4to commit

--- public/home.js
+++ public/home.js
@@ -6,7 +6,7 @@ async function loadCountries() {
   try {
     const countries = await fetch(`${window.API_BASE_URL}/api/countries`).then(r => r.json());
     grid.innerHTML = Object.entries(countries).map(([code, country]) => `
-      <a class="card" href="/acquirer.html?country=${code}">
+      <a class="card" href="./acquirer.html?country=${code}">
         ${country.logo
           ? `<img class="card-logo" src="${country.logo}" alt="${country.name}" onerror="this.replaceWith(Object.assign(document.createElement('span'),{className:'flag',textContent:'${FLAGS[code] || ''}'}))">`
           : `<span class="flag">${FLAGS[code] || ''}</span>`}
